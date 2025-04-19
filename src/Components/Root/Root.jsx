efd0c3302ceb4c667477f907a189f3479310eaf7
hosen rabby <tahmid.tuhin.3@gmail.com>
2025-04-19 23:46:30 +0600

    Routes url problem solved and footer 100vh set

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -8,7 +8,9 @@ const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <div className='min-h-[calc(100vh-364px)]'>
             <Outlet></Outlet>
+            </div>
             <Footer></Footer>
         </div>
     );
